85b731ad2834fe83d9faccf04daf645024152162
Charles Vandendriessche <chvandendriessche@neomnia.net>
2025-10-17 16:51:54 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 // server.js
-const { createServer } = require('http');
-const next = require('next');
+import { createServer } from 'http';
+import { parse } from 'url';
+import next from 'next';
 
-const dev = false;
-const port = parseInt(process.env.PORT || '3000', 10);
-const host = process.env.HOST || '0.0.0.0';
-
+const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  createServer((req, res) => handle(req, res)).listen(port, host, () => {
-    console.log(`> Ready on http://${host}:${port}`);
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(process.env.PORT || 3000, () => {
+    console.log(`> Ready on http://localhost:${process.env.PORT || 3000}`);
   });
 });
